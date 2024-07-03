d4ab427518ffddafc39c76bf871c11354804112d
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-07-03 18:52:47 +0500

    feat: add json handler and use interfaces

--- main.go
+++ main.go
@@ -1,70 +1,108 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"net/http"
 )
+
+type RedirectHandler struct {
+	config   map[string]string
+	fallback http.Handler
+}
+
+func NewRedirectHandler(config map[string]string, fallback http.Handler) *RedirectHandler {
+	return &RedirectHandler{
+		config:   config,
+		fallback: fallback,
+	}
+}
+
+func (h *RedirectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	if dest, ok := h.config[path]; ok {
+		http.Redirect(w, r, dest, http.StatusSeeOther)
+		return
+	}
+
+	h.fallback.ServeHTTP(w, r)
+}
+
+func LoadConfigFromYaml(bytes []byte) (map[string]string, error) {
+	var slice []PathUrls
+	err := yaml.Unmarshal(bytes, &slice)
+	if err != nil {
+		return nil, err
+	}
+	pathToUrls := make(map[string]string)
+	for _, path := range slice {
+		pathToUrls[path.Path] = path.Url
+	}
+	return pathToUrls, nil
+}
 
 type PathUrls struct {
 	Path string `yaml:"path"`
 	Url  string `yaml:"url"`
 }
 
+type JsonConfig struct {
+	Config map[string]string `json:"config"`
+}
+
+func LoadConfigFromJson(bytes []byte) (map[string]string, error) {
+	var config JsonConfig
+	err := json.Unmarshal(bytes, &config)
+	if err != nil {
+		return nil, err
+	}
+	return config.Config, nil
+}
+
 func main() {
-	dictionary := map[string]string{
-		"/rick":   "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-		"/google": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+	yml := `
+- path: /rick
+  url: https://www.youtube.com/watch?v=dQw4w9WgXcQ
+- path: /google
+  url: https://www.youtube.com/watch?v=dQw4w9WgXcQ`
+
+	jsonConfig := `
+{
+	"config": {
+		"/rick": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+		"/google": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+	}
+}`
+
+	yamlConfig, err := LoadConfigFromYaml([]byte(yml))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	jsonConfigMap, err := LoadConfigFromJson([]byte(jsonConfig))
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", 301)
 	})
-	handlerFunc := HandlerFunc(dictionary, mux)
 
-	yml := `
-- path: /rick
-  url: https://www.youtube.com/watch?v=dQw4w9WgXcQ
-- path: /google
-  url: https://www.youtube.com/watch?v=dQw4w9WgXcQ`
-	ymlHandlerFunc := YmlHandler([]byte(yml), mux)
+	yamlHandler := NewRedirectHandler(yamlConfig, mux)
+	jsonHandler := NewRedirectHandler(jsonConfigMap, mux)
 
-	http.Handle("/", http.HandlerFunc(ymlHandlerFunc))
-	err2 := http.ListenAndServe(":8010", nil)
-	if err2 != nil {
-		panic(err2)
-	}
+	http.Handle("/yaml/", http.StripPrefix("/yaml/", yamlHandler))
+	http.Handle("/json/", http.StripPrefix("/json/", jsonHandler))
 
-	http.Handle("/", http.HandlerFunc(handlerFunc))
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
+
 	fmt.Println("Listening on port 8080")
-}
-
-func HandlerFunc(dict map[string]string, fallback http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path
-		if dest, ok := dict[path]; ok {
-			http.Redirect(w, r, dest, http.StatusSeeOther)
-			return
-		}
-
-		fallback.ServeHTTP(w, r)
-	}
-}
-
-func YmlHandler(bytes []byte, fallback http.Handler) http.HandlerFunc {
-	var slice []PathUrls
-	err := yaml.Unmarshal(bytes, &slice)
-	if err != nil {
-		return nil
-	}
-	pathToUrls := make(map[string]string)
-	for _, path := range slice {
-		pathToUrls[path.Path] = path.Url
-	}
-	return HandlerFunc(pathToUrls, fallback)
 }
