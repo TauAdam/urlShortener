ea70601116cdfc5a15077b348f85d73dc71ea36c
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-07-03 17:20:55 +0500

    feat: add handler based on map

--- main.go
+++ main.go
@@ -6,13 +6,31 @@ import (
 )
 
 func main() {
+	dictionary := map[string]string{
+		"/rick":   "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+		"/google": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", 301)
 	})
-	err := http.ListenAndServe(":8080", mux)
+	handler := HandlerFunc(dictionary, mux)
+	err := http.ListenAndServe(":8080", handler)
 	if err != nil {
 		return
 	}
 	fmt.Println("Listening on port 8080")
+
+}
+func HandlerFunc(dict map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := dict[path]; ok {
+			http.Redirect(w, r, dest, http.StatusSeeOther)
+			return
+		}
+
+		fallback.ServeHTTP(w, r)
+	}
 }
